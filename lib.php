b1f75d1625a29c9310e9521948a16e7d0cbb34d5
YIN GONG <max.g.laboratory@gmail.com>
2017-10-27 11:32:34 +1030

    plugin basic structure

---
+++ lib.php
@@ -0,0 +1,66 @@
+<?php
+// This file is part of Mindmap module for Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Defines the APIs used by report_forumgraph
+ *
+ * @package    report_snapp
+ * @copyright  2017 Max Gong <max.gong@unisa.edu.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die;
+
+/**
+ * This function extends the navigation with the report items
+ *
+ * @param navigation_node $navigation The navigation node to extend
+ * @param stdClass $course The course to object for the report
+ * @param stdClass $context The context of the course
+ */
+function report_forumgraph_extend_navigation_course($navigation, $course, $context) {
+    if (has_capability('report/snapp:view', $context)) {
+        $url = new moodle_url('/report/snapp/index.php', array('id'=>$course->id));
+        $navigation->add(get_string('pluginname', 'report_snapp'), $url, navigation_node::TYPE_SETTING, null, null, new pix_icon('i/report', ''));
+    }
+}
+
+/**
+ * Get all forums in the course for use in dropdown list
+ *
+ * @param int $cid course id
+ * @return array all forum in the course
+ */
+function report_get_forums($cid) {
+    global $DB;
+    $forumoptions = array();
+    if ($course = $DB->get_record('course', array('id'=>$cid))) {
+        if ($forums = $DB->get_records('forum', array('course'=>$cid), 'id DESC')) {
+            foreach ($forums as $forum) {
+                $cm = get_coursemodule_from_instance("forum", $forum->id, $cid);
+                $context = context_module::instance($cm->id);
+                if (has_capability('mod/forum:addinstance', $context)) {
+                    if ($DB->record_exists('forum_discussions', array('forum'=>$forum->id))) {
+                        $forumoptions[$forum->id] = $forum->name;
+                    }
+                }
+            }
+        } else {
+            $forumoptions[0] = get_string('noforumincourse', 'report_forumgraph');
+        }
+    }
+    return $forumoptions;
+}
